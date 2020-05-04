85235c40fb1227f98671f007794693c223f49cae
Łukasz Żukowski <lukasz.zukowski@webinterpret.com>
2020-05-04 15:19:57 +0200

    Ensure Phone: split test_providing_phone_for_domestic_shipment_if_needed on boolean argument 'setting_default':  - test_providing_phone_for_domestic_shipment  - test_not_providing_phone_for_domestic_shipment

--- code/ensure_phone/tests.py
+++ code/ensure_phone/tests.py
@@ -23,21 +23,17 @@ def shipment():
     return s
 
 
-@pytest.mark.parametrize("phone, expected_phone, setting_default", [
-    (None, '07346923435', True),
-    ('', '07346923435', True),
-    ('0 7  344546234', '07344546234', False),
-    ('7 344546234', '07344546234', False),
-    ('344546234', '07346923435', True),
-    ('+ 44 72453', '072453', False),
-    ('0 044 72453', '072453', False),
-    ('0 44 72453', '07346923435', True),
-    ('+44 072453', '07346923435', True),
-    ('+44472453', '07346923435', True),
+@pytest.mark.parametrize("phone, expected_phone", [
+    (None, '07346923435'),
+    ('', '07346923435'),
+    ('344546234', '07346923435'),
+    ('0 44 72453', '07346923435'),
+    ('+44 072453', '07346923435'),
+    ('+44472453', '07346923435'),
 ])
-def test_providing_phone_for_domestic_shipment_if_needed(
-        phone, expected_phone, setting_default,
-        service, get_parameter_service, shipment):
+def test_providing_phone_for_domestic_shipment(
+        phone, expected_phone, service, get_parameter_service, shipment,
+):
     get_parameter_service.return_value = '07346923435'
     shipment.shipping_address.phone = phone
     assert shipment.is_domestic is True
@@ -45,9 +41,26 @@ def test_providing_phone_for_domestic_shipment_if_needed(
     service.provide(shipment, 'GB')
 
     assert shipment.shipping_address.phone == expected_phone
-    if setting_default:
-        calls = get_parameter_service.call_args_list
-        assert calls == [call('default_phone_for_domestic_GB')]
+    calls = get_parameter_service.call_args_list
+    assert calls == [call('default_phone_for_domestic_GB')]
+
+
+@pytest.mark.parametrize("phone, expected_phone", [
+    ('0 7  344546234', '07344546234'),
+    ('7 344546234', '07344546234'),
+    ('+ 44 72453', '072453'),
+    ('0 044 72453', '072453'),
+])
+def test_not_providing_phone_for_domestic_shipment(
+        phone, expected_phone, service, get_parameter_service, shipment,
+):
+    get_parameter_service.return_value = '07346923435'
+    shipment.shipping_address.phone = phone
+    assert shipment.is_domestic is True
+
+    service.provide(shipment, 'GB')
+
+    assert shipment.shipping_address.phone == expected_phone
 
 
 def test_not_providing_phone_for_domestic_shipment_if_no_default_set(
